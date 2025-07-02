62a44a2d437c40462cd6ec4e1a6a86577facb25f
Mohamed Khairy <mohamed.khairy.eg@gmail.com>
2025-07-02 13:36:02 +0300

    fix

--- app/Filament/Resources/TranslationResource.php
+++ app/Filament/Resources/TranslationResource.php
@@ -99,6 +99,7 @@ class TranslationResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
