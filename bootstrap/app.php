6e82beaab2da5f375ea2c2a893dc53f77e98c4d1
Mohamed Khairy <mohamed.khairy.eg@gmail.com>
2025-07-02 13:45:59 +0300

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,14 +6,23 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            return api_response(message: 'Unauthenticated.', statusCode: 401);
+        });
+
+        $exceptions->render(function (\Illuminate\Validation\ValidationException $e, $request) {
+            return api_response(
+                message: array_shift($e->errors()),
+                statusCode: 422
+            );
+        });
     })->create();
