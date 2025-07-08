3c4331df1247b198897a3bc98227ab3bd041dba2
Mohamed Khairy <mohamed.khairy.eg@gmail.com>
2025-07-08 15:36:16 +0300

    fix: enable navigation registration for SubscriptionResource

--- app/Filament/Resources/SubscriptionResource.php
+++ app/Filament/Resources/SubscriptionResource.php
@@ -21,7 +21,7 @@ class SubscriptionResource extends Resource
 
     public static function shouldRegisterNavigation(): bool
     {
-        return false;
+        return true;
     }
 
     public static function getModelLabel(): string
