766862f8a71cd123bb2e8206f4787ad8e3d32aca
Mohamed Khairy <mohamed.khairy.eg@gmail.com>
2025-07-14 15:57:04 +0300

    fix

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Console\Commands\CheckActiveSubscriptions;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('subscriptions:check', function () {
+    $this->comment(CheckActiveSubscriptions::quote());
+})->purpose('Display an subscriptions:check');
